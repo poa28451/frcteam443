72e1bbb7f533ad9a44252512ce17665dfa99927d
mark.bacchetti <mark.bacchetti@cc367a0c-fa41-f2f0-3753-bb7dbb63a56a>
2010-12-06 05:52:30 +0000

    First import. Code was tested and partially works. Still need work on PIDController.

---
+++ trunk/2010_Post_Season/Geisebot/src/edu/wpi/first/wpilibj/templates/Geisebot.java
@@ -0,0 +1,88 @@
+//----------------------------------------------------------------------------
+// Copyright (c) FIRST 2008. All Rights Reserved.
+// Open Source Software - may be modified and shared by FRC teams. The code
+// must be accompanied by the FIRST BSD license file in the root directory of
+// the project.
+//
+// File: Geisebot.java
+//
+// Description: This is the primary driver for the Team 443 robot. This is a
+// Post Season port to 2010. The original code was written in Labview for
+// the 2010 competition.
+//
+// Lead: TBD
+// ----------------------------------------------------------------------------
+
+package edu.wpi.first.wpilibj.templates;
+import  edu.wpi.first.wpilibj.IterativeRobot;
+
+// Import the 443 Java Library Logitech Dual Action Controller class
+import freelancelibj.LogitechDualActionController;
+
+// Import all tasks in the task package
+import tasks.*;
+
+public class Geisebot extends IterativeRobot {
+
+    private LogitechDualActionController Controller;
+    private Mecanum_Drive                Mecanum_Drive_Task;
+    private Kicker                       Kicker_Task;
+
+    /*
+    =========================================================================
+     * Robot Initialization
+    =========================================================================
+     */
+    public void robotInit() {
+
+        // Activate the Logitech_Dual_Action controller as the FL_Controller
+        Controller = new LogitechDualActionController(1);
+
+        // Mecanum drive constructor and initialization routine.
+        Mecanum_Drive_Task = new Mecanum_Drive();
+
+        // Kicker initialization
+        Kicker_Task = new Kicker();
+
+
+    }
+
+    /*
+    =========================================================================
+     * Autonomous
+    =========================================================================
+     */
+    public void autonomousPeriodic() {
+
+
+
+    }
+
+    /*
+    =========================================================================
+     * Driver - This method runs at 100 Hz (100 times a second)
+    =========================================================================
+     */
+    public void teleopPeriodic() {
+
+        /* Execute the mecanum robot drive task. This tasks uses the joystick
+         * magnitude and direction as arguments and controls all four mecanum
+         * wheels on the robot.
+         */
+        Mecanum_Drive_Task.Perform_Teleop(Controller.getFL_Magnitude(),
+                                          Controller.getFL_Direction(),
+                                          Controller.getRightStickX(),
+                                          Controller.getButton1(),
+                                          Controller.getButton2(),
+                                          Controller.getButton3(),
+                                          Controller.getButton4());
+
+        /* Execute the kicker task with controller inputs. TODO! Need to
+         * confirm buttons!
+         */
+        Kicker_Task.Perform_Teleop(Controller.getL1(),
+                                   Controller.getL2(),
+                                   Controller.getButton10());
+
+    }
+}
---
+++ trunk/2010_Post_Season/Geisebot/src/tasks/Kicker.java
@@ -0,0 +1,44 @@
+//----------------------------------------------------------------------------
+// Copyright (c) FIRST 2008. All Rights Reserved.
+// Open Source Software - may be modified and shared by FRC teams. The code
+// must be accompanied by the FIRST BSD license file in the root directory of
+// the project.
+//
+// File: Kicker.java
+//
+// Description: Primary task logic for the kicking mechanism
+//
+// Lead: TBD
+// ----------------------------------------------------------------------------
+
+package tasks;
+
+import edu.wpi.first.wpilibj.Jaguar;
+
+
+public class Kicker {
+
+    private Jaguar Kicker;
+
+    public Kicker(){
+
+        Kicker = new Jaguar(4,5);
+
+    }
+
+    public void Perform_Teleop(boolean Fast_Kick_Button,
+                               boolean Slow_Kick_Button,
+                               boolean Reverse_Kick_Button){
+
+        if(Fast_Kick_Button){
+            Kicker.set(0.9);
+        }
+        else if(Slow_Kick_Button){
+            Kicker.set(0.5);
+        }
+        else if(Reverse_Kick_Button){
+            Kicker.set(-0.2);
+        }
+    }
+
+}
