6634b4b6468707c10eee5dfb8f8f0116820a623d
mark.bacchetti <mark.bacchetti@cc367a0c-fa41-f2f0-3753-bb7dbb63a56a>
2011-01-05 02:13:41 +0000

    Added a constants class where all hardware constants are stored in a centralized place.

---
+++ 2010_Post_Season/Geisebot/src/tasks/Constants.java
@@ -0,0 +1,42 @@
+//----------------------------------------------------------------------------
+//
+// FREELANCE ROBOTICS - TEAM 443
+//
+// File: Constants.java
+//
+// Description:
+//
+// 01/01 - ...
+// ----------------------------------------------------------------------------
+package tasks;
+
+public final class Constants {
+
+    // Joystick channel
+    public final static int JOYSTICK1_CHNL = 1;
+
+    // cRIO DIO Slot
+    public final static int DIO_SLOT = 4;
+
+    // cRIO Analog Slot
+    public final static int ANLG_SLOT = 1;
+
+    // Jaguar channels for all four wheels
+    public final static int FL_CHNL = 1;
+    public final static int FR_CHNL = 4;
+    public final static int RL_CHNL = 2;
+    public final static int RR_CHNL = 3;
+
+    // Gyro channel
+    public final static int GYRO_CHNL = 1;
+
+    // Jaguar kicker channel
+    public final static int KICKER_CHNL = 5;
+
+    // Motor inversion
+    public final static boolean FL_INVERT = false;
+    public final static boolean FR_INVERT = true;
+    public final static boolean RL_INVERT = false;
+    public final static boolean RR_INVERT = true;
+
+}
